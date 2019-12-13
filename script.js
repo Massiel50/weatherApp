2d857d1f23583ba21747e4c8cc34bd64a1d10d58
Massiel50 <massielescano@yahoo.com>
2019-12-12 23:42:59 -0500

    moved uv func call inside getweather func

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 
 $(document).ready(function(){
-    // api.openweathermap.org/data/2.5/weather?q={city name}
-    let city;
-    const weatherAPI = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=";
+    // api.openweathermap.org/data/2.5/weather?q={searchValue name}
+    const weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
     const weatherAPIKey = "3be2b2b6acc21e3760901d15acf91f72";
     // "1177fbd51295e22fa6e70b5d7b978a5c";
 
     // uses searched city name to get city climate data
     function currentweather(searchValue){
         $.ajax({
-            url: weatherAPI + weatherAPIKey,
+            url: weatherAPI + searchValue + "&appid=" + weatherAPIKey,
             method: 'GET'
         }).then(function(response){
             console.log(response);
@@ -21,11 +20,14 @@ $(document).ready(function(){
     // grabs forcast data from api
     function getForeCast (searchValue){
         $.ajax({
-            url: "api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml" + weatherAPIKey,
+            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + weatherAPIKey,
             method: 'GET'
         }).then(function(response){
             console.log(response);
 
+            $(".forecast").html("<p> feels like " + response.list[0].main.feels_like + "</p>");
+
+            getUV(response.coord.lat, response.coord.lon)
         })
     }
     // uses lat and lon data to determine uv index with api data
@@ -36,18 +38,18 @@ $(document).ready(function(){
         }).then(function(response){
             console.log(response);
 
-            $(".uv").html("<p>uv" + response.value + "</p>");
+            $(".uv").html("<p> this is the uv " + response.value + "</p>");
         })
     }
 
 
     // click on search button to grab city name
     $(".searchButton").on("click", function(){
-        var city = "london";
+        var searchValue = $("#search-value").val();
 
-        currentweather(city);
-        getForeCast(city);
-
+        currentweather(searchValue);
+        getForeCast(searchValue);
+       
     })
 });
 
